e7f685298295c243acf934464ffc48c2de214a1b
Devin G <engy100x@gmail.com>
2017-04-17 16:19:20 -0700

    function works to display 3 random images from array

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 'use strict'
 
 console.log('lets goooo');
+
+
+var catalogProducts = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg'];
+
+var firstOption = document.getElementById('firstOption');
+var secondOption = document.getElementById('secondOption');
+var thirdOption = document.getElementById('thirdOption');
+
+function selectVotePhoto() {
+  var randomPhoto = catalogProducts[Math.floor(Math.random() * catalogProducts.length)];
+   return randomPhoto;
+}
+
+firstOption.innerHTML = '<img src="imgs/' + selectVotePhoto() + '" />';
+secondOption.innerHTML = '<img src="imgs/' + selectVotePhoto() + '" />';
+thirdOption.innerHTML = '<img src="imgs/' + selectVotePhoto() +'" />';
